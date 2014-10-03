eea417166ab5ad704d39f3ff206db33e53fbb8fd
ashokslsk <ashokslsk@gmail.com>
2014-10-03 15:04:11 +0530

    Autheticated android JSON feed

---
+++ src/ash/dev/pullparsers/HttpManager.java
@@ -0,0 +1,88 @@
+package ash.dev.pullparsers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.util.Base64;
+
+
+public class HttpManager {
+
+	public static String getData(String uri) {
+		
+		BufferedReader reader = null;
+		
+		try {
+			URL url = new URL(uri);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			
+			StringBuilder sb = new StringBuilder();
+			reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			
+			String line;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			
+			return sb.toString();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+					return null;
+				}
+			}
+		}
+	}
+	
+		public static String getData(String uri,String Username, String Password) {
+			
+			BufferedReader reader = null;
+			
+			byte[] loginBytes = (Username + ":" + Password).getBytes();
+			StringBuilder loginBuilder = new StringBuilder()
+			.append("Basic ")
+			.append(Base64.encodeToString(loginBytes, Base64.DEFAULT));
+			
+			try {
+				URL url = new URL(uri);
+				HttpURLConnection con = (HttpURLConnection) url.openConnection();
+				
+				con.addRequestProperty("Authorization", loginBuilder.toString());
+				
+				StringBuilder sb = new StringBuilder();
+				reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				
+				String line;
+				while ((line = reader.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+				
+				return sb.toString();
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			} finally {
+				if (reader != null) {
+					try {
+						reader.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+						return null;
+					}
+				}
+			}
+		
+	}
+	
+}
